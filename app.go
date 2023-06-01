6d8dc97cf99e1c07108f507d7af269c0db4b21d2
JesusRamirezN <jesusramirezn.tech@gmail.com>
2023-06-01 15:24:08 -0600

    Fixed hello route

--- app.go
+++ app.go
@@ -11,7 +11,7 @@ import (
 )
 
 func hiHandler(w http.ResponseWriter, r *http.Request) {
-    if r.URL.Path != "/" {
+    if r.URL.Path != "/hello" {
         http.Error(w, "404 not found.", http.StatusNotFound)
         return
     }
